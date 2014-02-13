6dd955a82389a6e6ca2d9d4d7b34b6366d2d5e8f
Minsoo Park <minsoo1003@gmail.com>
2014-02-14 02:55:09 +0900

    url changed

--- src/sgen/backup/dr/network/RequestConfig.java
+++ src/sgen/backup/dr/network/RequestConfig.java
@@ -1,5 +1,5 @@
 package sgen.backup.dr.network;
 
 public class RequestConfig {
-    public static final String BASE_URL = "http://www.backup-doctor.appspot.com";
+    public static final String BASE_URL = "http://backup-doctor.herokuapp.com";
 }
